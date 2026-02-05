eb85705a510e1149525eb1bde1a5243acb400f0b
TimmyMalatjie <timzmalatjie@gmail.com>
2026-02-05 20:59:55 +0200

    Run migrations on Vercel cold start

--- api/index.py
+++ api/index.py
@@ -1,6 +1,17 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timmy_gym_demo.settings')
+
+django.setup()
+
+# Run migrations on cold start for serverless
+try:
+	from django.core.management import call_command
+	call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
+except Exception:
+	# Avoid crashing the function on migration errors
+	pass
 
 app = get_wsgi_application()
